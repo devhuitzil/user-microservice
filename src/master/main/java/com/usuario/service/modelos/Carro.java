d82f0afeabaf090efafb38322e889c3bdbc8d0a3
devhuitzil <devhuitzil@gmail.com>
2022-12-08 12:15:56 -0600

    cambios usuario-service

---
+++ src/master/main/java/com/usuario/service/modelos/Carro.java
@@ -0,0 +1,17 @@
+package com.usuario.service.modelos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Carro {
+	
+	@Getter @Setter private String marca;
+	@Getter @Setter private String modelo;
+	@Getter @Setter private int usuarioId;
+
+		public Carro() {
+		super();
+	
+		
+	}
+		}
